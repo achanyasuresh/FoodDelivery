935d56a05fcff26dab65f1a8a9fab7502ac2e658
achanyasuresh <achanyapsuresh96@gmail.com>
2024-09-30 02:05:56 +1300

    changes

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -13,15 +13,13 @@ const Verify = () => {
   const navigate = useNavigate();
 
   const verifyPayment = async () => {
-    const response = await axios.post(url + "/api/order/verify", { success, orderId });
+    const response = await axios.get(url + `/api/order/verify?success=${success}&orderId=${orderId}`);
     if (response.data.success) {
-      navigate("/myorders");
+        navigate("/");
+    } else {
+        navigate("/");
     }
-    else {
-      navigate("/")
-    }
-  }
-
+};
   useEffect(() => {
     verifyPayment();
   }, [])
