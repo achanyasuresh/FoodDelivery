80e6bc8d21e421d43fa8db55214cd3c8f871a0a4
achanyasuresh <76219606+achanyasuresh@users.noreply.github.com>
2024-10-02 03:33:20 +1300

    Update Verify.jsx

--- src/pages/Verify/Verify.jsx
+++ src/pages/Verify/Verify.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import React, { useContext, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { storeContext } from '../../data/storeContext'
 import './Verify.css'
 
 const Verify = () => {
-  const { url } = useContext(storeContext)
+  const url  = "https://fooddeliverybackend-29bk.onrender.com"
   const [searchParams, setSearchParams] = useSearchParams();
   const success = searchParams.get("success")
   const orderId = searchParams.get("orderId")
@@ -13,7 +12,7 @@ const Verify = () => {
   const navigate = useNavigate();
 
   const verifyPayment = async () => {
-    const response = await axios.get(url + `/api/order/verify?success=${success}&orderId=${orderId}`);
+    const response = await axios.post(url + `/api/order/verify?success=${success}&orderId=${orderId}`);
     if (response.data.success) {
         navigate("/");
     } else {
